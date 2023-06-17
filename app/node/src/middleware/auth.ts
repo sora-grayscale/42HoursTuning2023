b0ac30f2add23b75079b2796a6d79072fb449f73
susui <96704644+sora-grayscale@users.noreply.github.com>
2023-06-18 00:09:45 +0900

    modo

--- app/node/src/middleware/auth.ts
+++ app/node/src/middleware/auth.ts
@@ -1,38 +1,3 @@
-// import { getSessionBySessionId } from "../routes/session/repository";
-// import express from "express";
-// 
-// export const checkAuthMiddleware = async (
-//   req: express.Request,
-//   res: express.Response,
-//   next: express.NextFunction
-// ) => {
-//   // ログインAPIはチェックしない
-//   if (req.method === "POST" && req.path === "/api/v1/session") {
-//     return next();
-//   }
-// 
-//   try {
-//     if (req.cookies?.SESSION_ID === undefined) {
-//       res.status(401).json({ message: "Unauthorized" });
-//       console.warn("cookies or session id is empty");
-//       return;
-//     }
-// 
-//     const session = await getSessionBySessionId(req.cookies.SESSION_ID);
-//     if (!session) {
-//       res.status(401).json({ message: "Unauthorized" });
-//       console.warn("invalid session id is set");
-//       return;
-//     }
-// 
-//     console.log("user has a valid session");
-//     req.headers["X-DA-USER-ID"] = session.userId;
-//     next();
-//   } catch (e) {
-//     next(e);
-//   }
-// };
-
 import { getSessionBySessionId } from "../routes/session/repository";
 import express from "express";
 
@@ -47,14 +12,13 @@ export const checkAuthMiddleware = async (
   }
 
   try {
-    const sessionId = req.cookies?.SESSION_ID;
-    if (!sessionId) {
+    if (req.cookies?.SESSION_ID === undefined) {
       res.status(401).json({ message: "Unauthorized" });
       console.warn("cookies or session id is empty");
       return;
     }
 
-    const session = await getSessionBySessionId(sessionId);
+    const session = await getSessionBySessionId(req.cookies.SESSION_ID);
     if (!session) {
       res.status(401).json({ message: "Unauthorized" });
       console.warn("invalid session id is set");
@@ -68,4 +32,3 @@ export const checkAuthMiddleware = async (
     next(e);
   }
 };
-
