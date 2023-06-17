c7f12d4b6994193a5dd230343bd2123657de6cd5
susui <96704644+sora-grayscale@users.noreply.github.com>
2023-06-17 22:13:05 +0900

    a

--- app/node/src/routes/match-groups/repository.ts
+++ app/node/src/routes/match-groups/repository.ts
@@ -4,15 +4,54 @@ import { MatchGroup, MatchGroupDetail, User } from "../../model/types";
 import { getUsersByUserIds } from "../users/repository";
 import { convertToMatchGroupDetail } from "../../model/utils";
 
-
 export const hasSkillNameRecord = async (
   skillName: string
 ): Promise<boolean> => {
   const [rows] = await pool.query<RowDataPacket[]>(
-    "SELECT skill_id FROM skill WHERE skill_name = ? LIMIT 1",
+    "SELECT * FROM skill WHERE EXISTS (SELECT * FROM skill WHERE skill_name = ?)",
     [skillName]
   );
   return rows.length > 0;
+};
+
+export const getUserIdsBeforeMatched = async (
+  userId: string
+): Promise<string[]> => {
+  const [matchGroupIdRows] = await pool.query<RowDataPacket[]>(
+    "SELECT match_group_id FROM match_group_member WHERE user_id = ?",
+    [userId]
+  );
+  if (matchGroupIdRows.length === 0) {
+    return [];
+  }
+
+  const [userIdRows] = await pool.query<RowDataPacket[]>(
+    "SELECT user_id FROM match_group_member WHERE match_group_id IN (?)",
+    [matchGroupIdRows]
+  );
+
+  return userIdRows.map((row) => row.user_id);
+};
+
+export const insertMatchGroup = async (matchGroupDetail: MatchGroupDetail) => {
+  await pool.query<RowDataPacket[]>(
+    "INSERT INTO match_group (match_group_id, match_group_name, description, status, created_by, created_at) VALUES (?, ?, ?, ?, ?, ?)",
+    [
+      matchGroupDetail.matchGroupId,
+      matchGroupDetail.matchGroupName,
+      matchGroupDetail.description,
+      matchGroupDetail.status,
+      matchGroupDetail.createdBy,
+      matchGroupDetail.createdAt,
+    ]
+  );
+
+  for (const member of matchGroupDetail.members) {
+    await pool.query<RowDataPacket[]>(
+      "INSERT INTO match_group_member (match_group_id, user_id) VALUES (?, ?)",
+      [matchGroupDetail.matchGroupId, member.userId]
+    );
+  }
 };
 
 export const getMatchGroupDetailByMatchGroupId = async (
@@ -20,58 +59,59 @@ export const getMatchGroupDetailByMatchGroupId = async (
   status?: string
 ): Promise<MatchGroupDetail | undefined> => {
   let query =
-    "SELECT mg.match_group_id, mg.match_group_name, mg.description, mg.status, mg.created_by, mg.created_at, mm.user_id " +
-    "FROM match_group AS mg " +
-    "JOIN match_group_member AS mm ON mg.match_group_id = mm.match_group_id " +
-    "WHERE mg.match_group_id = ?";
+    "SELECT match_group_id, match_group_name, description, status, created_by, created_at FROM match_group WHERE match_group_id = ?";
   if (status === "open") {
-    query += " AND mg.status = 'open'";
+    query += " AND status = 'open'";
   }
-  const [rows] = await pool.query<RowDataPacket[]>(query, [matchGroupId]);
-  if (rows.length === 0) {
+  const [matchGroup] = await pool.query<RowDataPacket[]>(query, [matchGroupId]);
+  if (matchGroup.length === 0) {
     return;
   }
 
-  const members: User[] = rows.map((row) => {
-    const { user_id, ...rest } = row;
+  const [matchGroupMemberIdRows] = await pool.query<RowDataPacket[]>(
+    "SELECT user_id FROM match_group_member WHERE match_group_id = ?",
+    [matchGroupId]
+  );
+  const matchGroupMemberIds: string[] = matchGroupMemberIdRows.map(
+    (row) => row.user_id
+  );
+
+  const searchedUsers = await getUsersByUserIds(matchGroupMemberIds);
+  // SearchedUserからUser型に変換
+  const members: User[] = searchedUsers.map((searchedUser) => {
+    const { kana: _kana, entryDate: _entryDate, ...rest } = searchedUser;
     return rest;
   });
-  const matchGroupDetail = convertToMatchGroupDetail(rows[0]);
-  matchGroupDetail.members = members;
+  matchGroup[0].members = members;
 
-  return matchGroupDetail;
+  return convertToMatchGroupDetail(matchGroup[0]);
 };
 
-export const insertMatchGroup = async (matchGroupDetail: MatchGroupDetail) => {
-  const conn = await pool.getConnection();
-  await conn.beginTransaction();
+export const getMatchGroupIdsByUserId = async (
+  userId: string
+): Promise<string[]> => {
+  const [matchGroupIds] = await pool.query<RowDataPacket[]>(
+    "SELECT match_group_id FROM match_group_member WHERE user_id = ?",
+    [userId]
+  );
+  return matchGroupIds.map((row) => row.match_group_id);
+};
 
-  try {
-    await conn.query(
-      "INSERT INTO match_group (match_group_id, match_group_name, description, status, created_by, created_at) VALUES (?, ?, ?, ?, ?, ?)",
-      [
-        matchGroupDetail.matchGroupId,
-        matchGroupDetail.matchGroupName,
-        matchGroupDetail.description,
-        matchGroupDetail.status,
-        matchGroupDetail.createdBy,
-        matchGroupDetail.createdAt,
-      ]
+export const getMatchGroupsByMatchGroupIds = async (
+  matchGroupIds: string[],
+  status: string
+): Promise<MatchGroup[]> => {
+  let matchGroups: MatchGroup[] = [];
+  for (const matchGroupId of matchGroupIds) {
+    const matchGroupDetail = await getMatchGroupDetailByMatchGroupId(
+      matchGroupId,
+      status
     );
-
-    for (const member of matchGroupDetail.members) {
-      await conn.query(
-        "INSERT INTO match_group_member (match_group_id, user_id) VALUES (?, ?)",
-        [matchGroupDetail.matchGroupId, member.userId]
-      );
+    if (matchGroupDetail) {
+      const { description: _description, ...matchGroup } = matchGroupDetail;
+      matchGroups = matchGroups.concat(matchGroup);
     }
-
-    await conn.commit();
-  } catch (error) {
-    await conn.rollback();
-    throw error;
-  } finally {
-    conn.release();
   }
-};
 
+  return matchGroups;
+};
