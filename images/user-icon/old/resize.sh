2029c66e73445414e6a56f89cc244971fd718445
susui <96704644+sora-grayscale@users.noreply.github.com>
2023-06-18 01:31:06 +0900

    resize

--- images/user-icon/old/resize.sh
+++ images/user-icon/old/resize.sh
@@ -1,13 +1,13 @@
-convert dog.png -resize 50% ../dog.png
-convert dragon.png -resize 50% ../dragon.png 
-convert goat.png -resize 50% ../goat.png 
-convert horse.png -resize 50% ../horse.png
-convert monkey.png -resize 50% ../monkey.png
-convert ox.png -resize 50% ../ox.png
-convert pig.png -resize 50% ../pig.png
-convert rabbit.png -resize 50% ../rabbit.png
-convert rat.png -resize 50% ../rat.png
-convert rooster.png -resize 50% ../rooster.png
-convert snake.png -resize 50% ../snake.png
-convert tiger.png -resize 50% ../tiger.png
+convert dog.png -resize 20% ../dog.png
+convert dragon.png -resize 20% ../dragon.png 
+convert goat.png -resize 20% ../goat.png 
+convert horse.png -resize 20% ../horse.png
+convert monkey.png -resize 20% ../monkey.png
+convert ox.png -resize 20% ../ox.png
+convert pig.png -resize 20% ../pig.png
+convert rabbit.png -resize 20% ../rabbit.png
+convert rat.png -resize 20% ../rat.png
+convert rooster.png -resize 20% ../rooster.png
+convert snake.png -resize 20% ../snake.png
+convert tiger.png -resize 20% ../tiger.png
 
