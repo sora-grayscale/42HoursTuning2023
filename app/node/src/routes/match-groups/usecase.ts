d70cc693b93d2f25acb444e71ccb746c6a54b772
susui <96704644+sora-grayscale@users.noreply.github.com>
2023-06-17 17:04:41 +0900

    fix

--- app/node/src/routes/match-groups/usecase.ts
+++ app/node/src/routes/match-groups/usecase.ts
@@ -38,42 +38,7 @@ export const createMatchGroup = async (
       return;
     }
     const candidate = await getUserForFilter();
-    if (
-      matchGroupConfig.departmentFilter !== "none" &&
-      !isPassedDepartmentFilter(
-        matchGroupConfig.departmentFilter,
-        owner.departmentName,
-        candidate.departmentName
-      )
-    ) {
-      console.log(`${candidate.userId} is not passed department filter`);
-      continue;
-    } else if (
-      matchGroupConfig.officeFilter !== "none" &&
-      !isPassedOfficeFilter(
-        matchGroupConfig.officeFilter,
-        owner.officeName,
-        candidate.officeName
-      )
-    ) {
-      console.log(`${candidate.userId} is not passed office filter`);
-      continue;
-    } else if (
-      matchGroupConfig.skillFilter.length > 0 &&
-      !matchGroupConfig.skillFilter.some((skill) =>
-        candidate.skillNames.includes(skill)
-      )
-    ) {
-      console.log(`${candidate.userId} is not passed skill filter`);
-      continue;
-    } else if (
-      matchGroupConfig.neverMatchedFilter &&
-      !(await isPassedMatchFilter(matchGroupConfig.ownerId, candidate.userId))
-    ) {
-      console.log(`${candidate.userId} is not passed never matched filter`);
-      continue;
-    } else if (members.some((member) => member.userId === candidate.userId)) {
-      console.log(`${candidate.userId} is already added to members`);
+    if (!isCandidateValid(matchGroupConfig, owner, candidate, members)) {
       continue;
     }
     members = members.concat(candidate);
@@ -94,11 +59,65 @@ export const createMatchGroup = async (
   return await getMatchGroupDetailByMatchGroupId(matchGroupId);
 };
 
+const isCandidateValid = async (
+  matchGroupConfig: MatchGroupConfig,
+  owner: UserForFilter,
+  candidate: UserForFilter,
+  members: UserForFilter[]
+): boolean => {
+  if (
+    matchGroupConfig.departmentFilter !== "none" &&
+    !isPassedDepartmentFilter(
+      matchGroupConfig.departmentFilter,
+      owner.departmentName,
+      candidate.departmentName
+    )
+  ) {
+    console.log(`${candidate.userId} is not passed department filter`);
+    return false;
+  }
+
+  if (
+    matchGroupConfig.officeFilter !== "none" &&
+    !isPassedOfficeFilter(
+      matchGroupConfig.officeFilter,
+      owner.officeName,
+      candidate.officeName
+    )
+  ) {
+    console.log(`${candidate.userId} is not passed office filter`);
+    return false;
+  }
+
+  if (
+    matchGroupConfig.skillFilter.length > 0 &&
+    !isPassedSkillFilter(matchGroupConfig.skillFilter, candidate.skillNames)
+  ) {
+    console.log(`${candidate.userId} is not passed skill filter`);
+    return false;
+  }
+
+  if (
+    matchGroupConfig.neverMatchedFilter &&
+    !(await isPassedMatchFilter(matchGroupConfig.ownerId, candidate.userId))
+  ) {
+    console.log(`${candidate.userId} is not passed never matched filter`);
+    return false;
+  }
+
+  if (members.some((member) => member.userId === candidate.userId)) {
+    console.log(`${candidate.userId} is already added to members`);
+    return false;
+  }
+
+  return true;
+};
+
 const isPassedDepartmentFilter = (
   departmentFilter: string,
   ownerDepartment: string,
   candidateDepartment: string
-) => {
+): boolean => {
   return departmentFilter === "onlyMyDepartment"
     ? ownerDepartment === candidateDepartment
     : ownerDepartment !== candidateDepartment;
@@ -108,15 +127,25 @@ const isPassedOfficeFilter = (
   officeFilter: string,
   ownerOffice: string,
   candidateOffice: string
-) => {
+): boolean => {
   return officeFilter === "onlyMyOffice"
     ? ownerOffice === candidateOffice
     : ownerOffice !== candidateOffice;
 };
 
-const isPassedMatchFilter = async (ownerId: string, candidateId: string) => {
-  const userIdsBeforeMatched = await getUserIdsBeforeMatched(ownerId);
-  return userIdsBeforeMatched.every(
-    (userIdBeforeMatched) => userIdBeforeMatched !== candidateId
-  );
+const isPassedSkillFilter = (
+  skillFilter: string[],
+  candidateSkills: string[]
+): boolean => {
+  const candidateSkillsSet = new Set(candidateSkills);
+  return skillFilter.some((skill) => candidateSkillsSet.has(skill));
 };
+
+const isPassedMatchFilter = async (
+  ownerId: string,
+  candidateId: string
+): Promise<boolean> => {
+  const userIdsBeforeMatched = await getUserIdsBeforeMatched(ownerId);
+  return !userIdsBeforeMatched.includes(candidateId);
+};
+
