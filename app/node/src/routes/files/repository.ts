e7c59bc36750aac8e3503433c2e93a81a6f75446
susui <96704644+sora-grayscale@users.noreply.github.com>
2023-06-18 10:09:22 +0900

    tyotto change

--- app/node/src/routes/files/repository.ts
+++ app/node/src/routes/files/repository.ts
@@ -2,19 +2,34 @@ import { RowDataPacket } from "mysql2";
 import pool from "../../util/mysql";
 import { File } from "../../model/types";
 
+// export const getFileByFileId = async (
+//   fileId: string
+// ): Promise<File | undefined> => {
+//   const [file] = await pool.query<RowDataPacket[]>(
+//     "SELECT file_name, path FROM file WHERE file_id = ?",
+//     [fileId]
+//   );
+//   if (file.length === 0) {
+//     return;
+//   }
+// 
+//   return {
+//     fileName: file[0].file_name,
+//     path: file[0].path,
+//   };
+// };
+
 export const getFileByFileId = async (
   fileId: string
 ): Promise<File | undefined> => {
-  const [file] = await pool.query<RowDataPacket[]>(
-    "SELECT file_name, path FROM file WHERE file_id = ?",
-    [fileId]
-  );
+  const query = "SELECT file_name, path FROM file WHERE file_id = ?";
+  const [file] = await pool.query<RowDataPacket[]>(query, [fileId]);
+
   if (file.length === 0) {
-    return;
+    return ;
   }
 
-  return {
-    fileName: file[0].file_name,
-    path: file[0].path,
-  };
+  const { file_name: fileName, path } = file[0];
+  return { fileName, path };
 };
+
